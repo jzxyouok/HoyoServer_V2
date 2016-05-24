b75f811738963cf0f14b41edb7e4b9fc48102a27
zgy <sh15bg0110@zgy.local>
2016-05-24 09:36:39 +0800

    //Test

---
+++ HoyoServicer/ModelCenter/DataObject/Test+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+//
+//  Test+CoreDataProperties.swift
+//  HoyoServicer
+//
+//  Created by SH15BG0110 on 16/5/24.
+//  Copyright © 2016年 com.ozner.net. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Test {
+
+
+}
